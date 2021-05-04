ff2c9cad18391bd5c10528330e78623fd618549c
jasmine-emanouel <jemanouel@apple.com>
2021-05-04 17:48:15 +1000

    adding pages

---
+++ IOS_Dev/AddViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AddViewController.swift
+//  IOS_Dev
+//
+//  Created by Jasmine Emanouel on 4/5/21.
+//
+
+import Foundation
---
+++ IOS_Dev/TradeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TradeViewController.swift
+//  IOS_Dev
+//
+//  Created by Jasmine Emanouel on 4/5/21.
+//
+
+import Foundation
